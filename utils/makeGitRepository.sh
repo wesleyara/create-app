e4ba6d891a2553f3fb0c9a156fe777d414a8796b
wesleyara <wes1gom@gmail.com>
2022-11-21 09:18:52 -0300

    fix: change commands

--- utils/makeGitRepository.sh
+++ utils/makeGitRepository.sh
@@ -1,20 +1,16 @@
 #! /bin/bash
 
 if [ -d ".git" ]; then 
-  cd $1
   rm -rf .git
   git init
   git add .
   git commit -m "Initial commit by create-app"
   echo "Git repository created"
-  cd ..
   exit 1
 else 
-  cd $1
   git init
   git add .
   git commit -m "Initial commit by create-app"
   echo "Git repository created"
-  cd ..
   exit 1
 fi
